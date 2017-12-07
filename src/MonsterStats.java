494f3bb01f59ff2d5f57b1bb4ac75d0a4c6f92f9
timothywill <willt1646@my.uwstout.edu>
2017-12-07 10:43:29 -0600

    Fixing merge

--- src/MonsterStats.java
+++ src/MonsterStats.java
@@ -7,6 +7,10 @@ public class MonsterStats {
 	private int hp, ac, speed;
 	private String alignment, type, savingThrows, skills, senses, vulnerabilities, immunites, resistances, condition, languages; 
 	
+	private int CRHPMin[] = {  1,  7, 36, 50, 71,  86, 101, 116, 131, 146, 161, 176, 191, 206, 221, 236, 251, 266, 281, 296, 311, 326, 341, 356, 401, 446, 491, 536, 581, 626, 671, 716, 761, 806 };
+	private int CRHPMax[] = {  6, 35, 49, 70, 85, 100, 115, 130, 145, 160, 175, 190, 205, 220, 235, 250, 265, 280, 295, 310, 325, 340, 355, 400, 445, 490, 535, 580, 625, 670, 715, 760, 805, 850 };
+	private int CRACAvg[] = { 13, 13, 13, 13, 13,  13,  13,  14,  15,  15,  15,  16,  16,  17,  17,  17,  18,  18,  18,  18,  19,  19,  19,  19,  19,  19,  19,  19,  19,  19,  19,  19,  19,  19 };
+	
 	public MonsterStats() { }
 	
 	public void generateAbilities() {
@@ -145,5 +149,48 @@ public class MonsterStats {
 
 	public void setSpeed(int speed) {
 		this.speed = speed;
+	}
+	
+	
+	//Comment
+	private double calculateDefensiveCR(int armorClass, int hitPoints) {
+		if(hitPoints > CRHPMax[CRHPMax.length - 1]) {
+			return -1.0;
+		}
+		else {
+			double defCR = 0;
+			for(int i = 0; i < CRHPMin.length; i++) {
+				if(hitPoints >= CRHPMin[i] && hitPoints <= CRHPMax[i]) {
+					defCR = i;
+				}
+			}
+			defCR -= (CRACAvg[(int)defCR] - armorClass) / 2;
+			
+			if(defCR <= 0) {
+				defCR = 0.0;
+			}
+			else if(defCR <= 3) {
+				defCR = (1.0 / (Math.pow(2, 4 - defCR)));
+			}
+			else if(defCR >= 31.0) {
+				defCR = -1.0;
+			}
+			else {
+				defCR -= 3.0;
+			}
+			return defCR;
+		}
+	}
+	
+	private double calculateOffensiveCR(int attackBonus, int damagePerRound, int saveDC) {
+		double offCR = 0;
+		//TODO: Calculate offensive CR
+		return offCR;
+	}
+	
+	public double calculateCR(int armorClass, int hitPoints, int attackBonus, int damagePerRound, int saveDC) {
+		double defCR = calculateDefensiveCR(armorClass, hitPoints);
+		double offCR = calculateOffensiveCR(attackBonus, damagePerRound, saveDC);
+		return (int)Math.ceil((defCR + offCR) / 2.0);
 	}
 }
