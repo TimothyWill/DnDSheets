d7015d5e3191a3c9cdd8f77e914f0d5267e61ec9
Student <Student@144.13.136.48>
2017-12-10 14:23:43 -0600

    Saving Throw

--- src/characterSheet.java
+++ src/characterSheet.java
@@ -1,8 +1,74 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class characterSheet {
+	private int str, dex, con, intel, wis, cha;
+	private int strM, dexM, conM, intelM, wisM, chaM;
+	private String name, trait, race, clas, loc;
+	private int hp, ac;
+	private String save;
+	public int getStr() {
+		return str;
+	}
+	public int getStrM() {
+		return strM;
+	}
+	public int getDex() {
+		return dex;
+	}
+	public int getDexM() {
+		return dexM;
+	}
+	public int getCon() {
+		return con;
+	}
+	public int getConM() {
+		return conM;
+	}
+	public int getIntel() {
+		return intel;
+	}
+	public int getIntelM() {
+		return intelM;
+	}
+	public int getWis() {
+		return wis;
+	}
+	public int getWisM() {
+		return wisM;
+	}
+	public int getCha() {
+		return cha;
+	}
+	public int getChaM() {
+		return chaM;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public int getAc() {
+		return ac;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getTrait() {
+		return trait;
+	}
+	public String getRace() {
+		return race;
+	}
+	public String getClas() {
+		return clas;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public String getSave() {
+		return save;
+	}
 	public void character() {
 		/*************Name********************/
 		String line = null;
@@ -20,8 +86,8 @@ public class characterSheet {
 			// nextInt is exclusive. 
 			int randomIndex = rand.nextInt(array.size());
 
-			// Print your random name... 
-			System.out.print(array.get(randomIndex));//print to the jframe
+			// Set your random name...
+			name = array.get(randomIndex);
 			br.close();
 		}
 		catch(FileNotFoundException ex) {
@@ -34,7 +100,6 @@ public class characterSheet {
                 "Error reading file '" 
                 + file + "'");                  
         }
-		System.out.print(" a ");//print to the jframe
 		/*************Traits********************/
 		line = null;
 		file = "traits.txt";
@@ -51,8 +116,8 @@ public class characterSheet {
 			// nextInt is exclusive. 
 			int randomIndex = rand.nextInt(array.size());
 
-			// Print your random trait... 
-			System.out.print(array.get(randomIndex));//print to the jframe
+			// set your random trait...
+			trait = array.get(randomIndex);
 			br.close();
 		}
 		catch(FileNotFoundException ex) {
@@ -82,8 +147,8 @@ public class characterSheet {
 			// nextInt is exclusive. 
 			int randomIndex = rand.nextInt(array.size());
 
-			// Print your random race... 
-			System.out.print(array.get(randomIndex));//print to the jframe
+			// set your random race...
+			race = array.get(randomIndex);
 			br.close();
 		}
 		catch(FileNotFoundException ex) {
@@ -100,6 +165,7 @@ public class characterSheet {
 		/***************Class***************/
 		line = null;
 		file = "class.txt";
+		String cla[] = new String[12];
 		Random randd = new Random();
 		//declared outside so it can use that same number as the hitDice
 		//matches up the class with how much hp it should recieve
@@ -109,12 +175,13 @@ public class characterSheet {
 		try {
 			FileInputStream fs= new FileInputStream(file);
 			BufferedReader br = new BufferedReader(new InputStreamReader(fs));
-			ArrayList<String> array = new ArrayList<>();
+			int i = 0;
 			while((line = br.readLine()) != null) {
-				array.add(line);
+				cla[i] = line;
+				i++;
 			}
 			// Print your random class... 
-			System.out.print(array.get(randomIn));//print to the jframe
+			
 			br.close();
 		}
 		catch(FileNotFoundException ex) {
@@ -127,9 +194,10 @@ public class characterSheet {
                 "Error reading file '" 
                 + file + "'");                  
         }
+		clas = cla[randomIn];
 		//hitDice file will match up with the class
 		//determines hp of a character
-		file = "hitDice";
+		file = "hitDice.txt";
 		try {
 			FileInputStream fs= new FileInputStream(file);
 			BufferedReader br = new BufferedReader(new InputStreamReader(fs));
@@ -154,7 +222,7 @@ public class characterSheet {
         }
 		int hit = hitDice[randomIn];
 		String st[] = new String[12];
-		file = "savingThrow";
+		file = "savingThrow.txt";
 		try {
 			FileInputStream fs= new FileInputStream(file);
 			BufferedReader br = new BufferedReader(new InputStreamReader(fs));
@@ -176,8 +244,7 @@ public class characterSheet {
                 "Error reading file '" 
                 + file + "'");                  
         }
-		String save = st[randomIn];
-		System.out.print(" from ");//print to the jframe
+		save = st[randomIn];
 		/****************Location*****************/
 		line = null;
 		file = "place.txt";
@@ -195,7 +262,7 @@ public class characterSheet {
 			int randomIndex = rand.nextInt(array.size());
 
 			// Print your random location... 
-			System.out.println(array.get(randomIndex));//print to the jframe
+			loc = array.get(randomIndex);
 			br.close();
 		}
 		catch(FileNotFoundException ex) {
@@ -208,7 +275,6 @@ public class characterSheet {
                 "Error reading file '" 
                 + file + "'");                  
         }
-		System.out.print(" ");//print to the jframe
 		/***********Generates Abilities********/
 		Generator gen1 = new Generator();
 		int array[] = new int[6];
@@ -217,25 +283,19 @@ public class characterSheet {
 		for(int i=0; i<6; i++) {
 			mod[i]=(array[i]-10)/2;
 		}
-		int hp = hit+mod[2];
-		int ac = 10+mod[1];
-		//Prints out the stats******Needs to be put in jframe
-		System.out.println("Hp: "+hp+" Ac: "+ac);//print to the jframe
-		for(int i=0; i<6; i++) {
-			if(i==0)
-				System.out.print("Str: ");	/*					*/
-			else if(i==1)					/*					*/
-				System.out.print("Dex: ");	/*					*/
-			else if(i==2)					/*		Print		*/
-				System.out.print("Con: ");	/*		To			*/
-			else if(i==3)					/*		The			*/
-				System.out.print("Int: ");	/*		jframe		*/
-			else if(i==4)					/*					*/
-				System.out.print("Wiz: ");	/*					*/
-			else if(i==5)					/*					*/
-				System.out.print("Cha: ");	/*					*/
-			System.out.print(array[i]+" ("+mod[i]+")");//print to the jframe
+		hp = hit+mod[2];
+		ac = 10+mod[1];
+		str = array[0];
+		strM = mod[0];
+		dex = array[1];
+		dexM = mod[1];
+		con = array[2];
+		conM = mod[2];
+		intel = array[3];
+		intelM = mod[3];
+		cha = array[4];
+		chaM = mod[4];
 		}
-		System.out.println("\nSaving Throw Proficiencies: "+st);
+		//System.out.println("\nSaving Throw Proficiencies: "+st);//print to the jframe
 	}
-}
+
